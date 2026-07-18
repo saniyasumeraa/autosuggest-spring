b8ae062d19fca2b7b50339d0bf76ab33452bb57f
Saniya Sumera <saniyasumeraa@gmail.com>
2026-07-18 11:47:27 +0530

    Toggle User

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,2 +1,35 @@
 // alert("Hello From JS")
 console.log("Hello From JS")
+
+var users = [
+    {
+        "name" : "Jane Doe",
+        "gender" : "Female",
+        "img" : "jane.png"
+    },
+    {
+        "name" : "John Doe",
+        "gender" : "Male",
+        "img" : "john.png"
+    },
+]
+
+var id = 0;
+
+function toggleUser(){
+    id = (id + 1)%2
+    // get the elements
+
+    // 1: image
+    var userImage = document.getElementById("user-image");
+    userImage.src = users[id].img;
+
+    // 2: name
+    var userName = document.getElementById("user-name");
+    userName.innerHTML = users[id].name;
+
+    // 3: gender
+    var userGender = document.getElementById("user-gender");
+    userGender.innerHTML = users[id].gender;
+
+}
